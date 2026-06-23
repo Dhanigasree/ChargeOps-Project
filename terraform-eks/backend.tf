f106f2a17df87a5166160238f3462a179328437a
Dhaniga <dhanigasree03@gmail.com>
2026-06-23 15:43:21 +0530

    chore: enable terraform eks remote backend

--- terraform-eks/backend.tf
+++ terraform-eks/backend.tf
@@ -1,14 +1,10 @@
-# Local state is used for the initial EKS build.
-# After the state bucket is created, uncomment and run:
-# terraform init -migrate-state
-#
-# terraform {
-#   backend "s3" {
-#     bucket       = "chargeops-terraform-state-497676936148-ap-south-1"
-#     key          = "eks/terraform.tfstate"
-#     region       = "ap-south-1"
-#     encrypt      = true
-#     kms_key_id   = "alias/chargeops-terraform-state"
-#     use_lockfile = true
-#   }
-# }
+terraform {
+  backend "s3" {
+    bucket         = "chargeops-terraform-state-497676936148-ap-south-1"
+    key            = "eks/terraform.tfstate"
+    region         = "ap-south-1"
+    encrypt        = true
+    kms_key_id     = "alias/chargeops-terraform-state"
+    dynamodb_table = "chargeops-terraform-locks"
+  }
+}
